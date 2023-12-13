e0e84f0c2f185af7df3d8a29f56506b27c35b690
Lutaaya Jamil <jamillutaaya0@gmail.com>
2023-12-13 09:15:39 +0300

    database set well

---
+++ src/main/java/com/stark/shop/user/UserEntity.java
@@ -0,0 +1,84 @@
+package com.stark.shop.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    // id, username, password, fullname, email, date_created(timestamp)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String fullname;
+    private String email;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "date_created")
+    private Date dateCreated;
+
+    @PrePersist
+    protected void onCreate() {
+        dateCreated = new Date();
+    }
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String username, String password, String fullname, String email) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity [dateCreated=" + dateCreated + ", email=" + email + ", fullname=" + fullname + ", id=" + id
+                + ", password=" + password + ", username=" + username + "]";
+    }
+
+}
