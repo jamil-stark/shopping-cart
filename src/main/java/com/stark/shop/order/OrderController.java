8839c66da39ec82cccfd583759c9250537279bbc
Lutaaya Jamil <jamillutaaya0@gmail.com>
2024-01-02 15:45:56 +0300

    simple frontend

--- src/main/java/com/stark/shop/order/OrderController.java
+++ src/main/java/com/stark/shop/order/OrderController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("order/")
+@CrossOrigin(origins = "*")
 public class OrderController {
     private final OrderService orderService;
 
--- src/main/java/com/stark/shop/user/UserController.java
+++ src/main/java/com/stark/shop/user/UserController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users/")
+@CrossOrigin(origins = "*")
 public class UserController {
     private final UserService userService;
 
